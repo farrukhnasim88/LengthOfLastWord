42fe332e6fdc06090179ba7a28fc092099effab6
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-05-26 19:06:45 +1000

    Add project files.

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LengthOfLastWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Please Write sentence to see length of last word \n");
+            input= Console.ReadLine();
+             
+            int a = LengthOfLastWord(input);
+            Console.WriteLine("The length of last word is:  " + a);
+
+            static int LengthOfLastWord(string findLength)
+            {
+
+                string[] charWords = findLength.Split(' ');
+                string lastWord = charWords[charWords.Length - 1];
+
+                if ( lastWord != "")
+                {
+                    
+                    return lastWord.Length;
+                }
+                else
+                    return 0;
+            }
+
+
+
+
+
+
+        }
+    }
+}
